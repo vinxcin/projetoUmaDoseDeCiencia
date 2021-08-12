b45f9fde2628fde44a3f7a9b4fd3b597257ac44b
Vinicius <v.rosa@aluno.ifsp.edu.br>
2021-08-12 20:16:03 -0300

    commit inicial

--- projetoUmaDoseDeCiencia/resources/views/home.blade.php
+++ projetoUmaDoseDeCiencia/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Bem-Vindo') }}, {{ Auth::user()->name}}!</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,11 +13,12 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                        
+                    {{ __('Você está Logado!') }}
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
